cdb05ae79c0d80fa217e5453e48e80e02b4003b8
Logan McAnsh <logan@mcan.sh>
2025-01-30 21:39:42 -0500

    chore: use `@mcansh/react-router-vercel-vite-plugin`
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
+import { reactRouterVercelVitePlugin } from "@mcansh/react-router-vercel-vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { reactRouterDevTools } from "react-router-devtools";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ command, isSsrBuild }) => {
-  return {
-    plugins: [reactRouterDevTools(), tailwindcss(), reactRouter()],
-    build: {
-      cssMinify: true,
-      ssr: false,
-      rollupOptions: isSsrBuild ? { input: "./server/app.ts" } : undefined,
-    },
-    ssr: {
-      noExternal: command === "build" ? true : undefined,
-    },
-  };
+export default defineConfig({
+  plugins: [
+    reactRouterDevTools(),
+    tailwindcss(),
+    reactRouter(),
+    reactRouterVercelVitePlugin({ nodeJsRuntime: `nodejs22.x` }),
+  ],
 });
